7ed519f39d746e2ee0e3e8bcaaa9e31f78655d47
Nilakshan Raveendran <122280067+NilakshanRaveendran@users.noreply.github.com>
2025-11-06 16:45:51 +0530

    Update comment to remove bidirectional translation reference

--- Read.py
+++ Read.py
@@ -1,2 +1,2 @@
 #This is Transformer model, Done in Google's Colab with TensorFlow and Keras.
-#in this Model you can translate the english text to French also bidirectional.
+#in this Model you can translate the english text to French.
